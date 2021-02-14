6029d6e9f7347f2988c09c3374beb410725af0e8
Andreas Magnussen <andrefm@stud.ntnu.no>
2021-02-14 20:12:26 +0100

    Finished

--- backupV2.ps1
+++ backupV2.ps1
@@ -5,8 +5,8 @@
 #
 
 $disk = 'D:\'
-$backupPath = 'D:\backupFolder\'  
-$folder = 'C:\Users\Admin\Documents\'
+$backupPath = 'D:\backupFolder\'
+$folder = 'C:\Users\Admin\private\'
 $lastWriteTime = ( (Get-ChildItem $folder).LastWriteTime | Sort-Object -Bottom 1).ToString("dd/MM/yyyy/HH/mm")
 $lastWriteTimeParent = ((Get-Item $folder).LastWriteTime).ToString("dd/MM/yyyy/HH/mm")
 
@@ -26,12 +26,12 @@ function Add-DirIfNoPath {
         if (-Not (Test-Path $FullPath) ){
             New-Item -Type Directory $FullPath
         }
-        
-    } else{ 
+
+    } else{
         if (-Not (Test-Path $FullPath) ){
             New-Item -Path $FullPath -ItemType "file"
         }
-    }   
+    }
 }
 
 ############################ Controlled Folder Access ###############################################
@@ -56,6 +56,16 @@ if ( -Not ($protectedFolders -eq $folder) ) {
     Add-MpPreference -ControlledFolderAccessProtectedFolders $folder
 }
 
+########################### Encryption ########################################################
+
+$encryptFolder = Get-ChildItem $folder
+
+foreach ($file in $encryptFolder) {  
+    if ( -Not ($file).Attributes -match "Encrypted"){
+        cipher /e $file
+    }
+}
+
 ########################### Backup to external disk ############################################
 
 # If a file/dir has been deleten in the subfolder. Then the "lastWriteTime" needs to be added in an extra check
@@ -72,7 +82,7 @@ $lastBackupWriteTime = $feedBackFolder + "lastWriteTime.txt"
 Add-DirIfNoPath -FullPath $lastBackupWriteTime -Type "File"
 
 
-Set-ItemProperty $lastBackupWriteTime -Name IsReadOnly -Value $true    # Configure the access to be read-only        
+Set-ItemProperty $lastBackupWriteTime -Name IsReadOnly -Value $true    # Configure the access to be read-only
 
 # Retrive the date for when the last backup on the 'D' disk happened. This date was stored on a file.
 $lastBackup = Get-Content $lastBackupWriteTime
@@ -83,12 +93,12 @@ if ( (-Not $lastBackup) -or ($lastWriteTime -gt $lastBackup) ){
 
         # Try to connect to the disk if not connected
     if ( -Not (Test-Path $disk) ) {
-        Get-Disk -Number 1 | Get-Partition -PartitionNumber 2 | Add-PartitionAccessPath -AccessPath $disk  
-    } 
+        Get-Disk -Number 1 | Get-Partition -PartitionNumber 2 | Add-PartitionAccessPath -AccessPath $disk
+    }
 
         # The disk is connected
     if ( Test-Path $disk ) {
-        $backupSize = (Get-ChildItem $folder -Recurse | Measure-Object -Property Length -sum).Sum   
+        $backupSize = (Get-ChildItem $folder -Recurse | Measure-Object -Property Length -sum).Sum
         $availableDisk = (Get-PSDrive -Name D).Free
 
             #Check if there is avaliable space on the disk. If not, delete the oldest backup
@@ -104,15 +114,15 @@ if ( (-Not $lastBackup) -or ($lastWriteTime -gt $lastBackup) ){
 
         $backup = $backupPath + ( Get-Date -Format dd/MM/yyyy-HH-mm)
 
-        #Each backup will have its own folder. Call function to check for existense, and eventually create it. 
-        Add-DirIfNoPath -FullPath $backup -Type "Directory" 
+        #Each backup will have its own folder. Call function to check for existense, and eventually create it.
+        Add-DirIfNoPath -FullPath $backup -Type "Directory"
 
         Set-ItemProperty $lastBackupWriteTime -Name IsReadOnly -Value $false    # Allow access for write-permission
         $content = $folder + '*'
         Copy-Item $content $backup -Recurse
-        
+
         #Saves the backup-date and store the info in a file. This action will overwrite the last date.
-        $lastBackup = (Get-Date).ToString("dd/MM/yyyy/HH/mm") 
+        $lastBackup = (Get-Date).ToString("dd/MM/yyyy/HH/mm")
         Write-Output $lastBackup | Out-File -FilePath $lastBackupWriteTime
         Set-ItemProperty $lastBackupWriteTime -Name IsReadOnly -Value $true     # Limit the access back to read-only
 
@@ -122,7 +132,7 @@ if ( (-Not $lastBackup) -or ($lastWriteTime -gt $lastBackup) ){
         $errorFile = $feedBackFolder + "backupErrors.txt"
 
         # Call function to check if there is a file for managing disk errors. If not, it will be created
-        Add-DirIfNoPath -FullPath $errorFile -Type "File" 
+        Add-DirIfNoPath -FullPath $errorFile -Type "File"
 
         Write-Output $errorText | Out-File -FilePath $errorFile -Append
     }
